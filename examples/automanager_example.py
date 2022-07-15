7cf0033566773175e2e42b54090e50e30e06a475
charxhit <charchit.a00@gmail.com>
2022-07-15 16:37:43 +0530

    Added full-code examples

---
+++ examples/automanager_example.py
@@ -0,0 +1,78 @@
+from recaptcha_manager import AutoManager, AntiCaptcha, TwoCaptcha
+import recaptcha_manager
+import random
+import time
+
+
+def exc_handler(exc):
+    """
+    This exc_handler ignores all exceptions and retries the HTTP requests that caused them till they succeed
+    """
+
+    print(f'Exception {exc} encountered: Request will be automatically retried!')
+    # raise
+
+def main(total_run):
+
+    # Create a queue and an Automanager instance
+    request_queue = recaptcha_manager.generate_queue()
+    manager = AutoManager.create(request_queue=request_queue, url=url, web_key=site_key, captcha_type=captcha_type)
+
+    # Create a service, and spawn a service process. Uncomment to use 2Captcha instead
+    # service = TwoCaptcha.create_service(key=api_key, request_queue=request_queue)
+    service = AntiCaptcha.create_service(key=api_key, request_queue=request_queue)
+    service_proc = service.spawn_process(exc_handler=exc_handler)
+
+    count = 0
+    while True:
+        # Check if the service process is still working
+        service.get_exception()
+
+        # Signal Automanager to send optimal number of requests, and then wait till one is solved
+        manager.send_request(initial=2)
+        try:
+            captcha_answer = manager.get_request(max_block=120)
+        except recaptcha_manager.exceptions.Exhausted:
+            break  # Manager is no longer usable
+        except recaptcha_manager.exceptions.TimeOutError:
+            # This may be the first few captcha requests (which take longer) or some issue is there with solving
+            # service. So, we continue to top where it will check for errors in captcha solving service and send more
+            # requests
+            continue
+
+        # captcha_answer is a dictionary containing useful information about the solved captcha, with the actual
+        # token under key 'answer'
+        print(captcha_answer)
+        token = captcha_answer['answer']
+
+        # You can now use this token to submit forms, etc. Sleep is used here instead to mock that
+        time.sleep(random.randint(5, 10))
+
+        # Print out the avg time your function waits to receive a captcha answer. This will reduce over time
+        print(f"Average time spent waiting for a captcha answer : {manager.get_waiting_time()}")
+
+        # Print out some other statistics regarding captcha solving as well
+        print(f"Average time solving service takes to solve the captcha : {manager.get_solving_time()}")
+        print(f"One captcha is requested every {manager.get_use_rate()}s from the manager by your program\n")
+
+        # Stop manager if limit reached. Manager will completely stop only after all registered requests are solved
+        # as well (Use force_stop() to stop manager immediately instead).
+        count += 1
+        if count == total_run:
+            print('Manager stopped')
+            manager.stop()
+
+    # Stop the service process
+    service.stop()
+    service_proc.join()
+
+
+site_key = '6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-'
+api_key = ''
+captcha_type = 'v2'
+url = 'https://www.google.com/recaptcha/api2/demo'
+total_runs = 40
+
+# Remember to protect your main with this clause!
+if __name__ == "__main__":
+    main(total_runs)
---
+++ examples/manualmanager_example.py
@@ -0,0 +1,79 @@
+from recaptcha_manager import ManualManager, AntiCaptcha, TwoCaptcha
+import recaptcha_manager
+import random
+import time
+
+
+def exc_handler(exc):
+    """
+    This exc_handler ignores all exceptions and retries the HTTP requests that caused them till they succeed
+    """
+
+    print(f'Exception {exc} encountered: Request will be automatically retried!')
+
+def main(total_run):
+
+    # Create a queue and an Manualmanager instance
+    request_queue = recaptcha_manager.generate_queue()
+    manager = ManualManager.create(request_queue=request_queue)
+
+    # Create a service, and spawn a service process. Uncomment to use 2Captcha instead
+    # service = TwoCaptcha.create_service(key=api_key, request_queue=request_queue)
+    service = AntiCaptcha.create_service(key=api_key, request_queue=request_queue)
+    service_proc = service.spawn_process(exc_handler=exc_handler)
+
+    # Pre-send one captcha request before we enter the loop
+    batch_id = manager.send_request(number=1, url=url, web_key=site_key, captcha_type=captcha_type)
+    count = 0
+    while True:
+        # Check if the service process is still working
+        service.get_exception()
+
+        # Get the captcha request using the id that we got
+        try:
+            captcha_answer = manager.get_request(batch_id=batch_id, max_block=120)
+        except recaptcha_manager.exceptions.Exhausted:
+            break  # Manager is no longer usable
+        except (recaptcha_manager.exceptions.TimeOutError, recaptcha_manager.exceptions.EmptyError):
+            # This may be the first few captcha requests (which take longer) or some issue is there with solving
+            # service. So, we continue to top where it will check for errors in captcha solving service and send more
+            # requests
+            continue
+
+        # captcha_answer is a dictionary containing useful information about the solved captcha, with the actual
+        # token under key 'answer'
+        print(captcha_answer)
+        token = captcha_answer['answer']
+
+        # Pre-send another captcha request before we do some other task to reduce future waiting time. Make sure to
+        # check whether the batch_id returned is not None. This can happen if you attempt to send requests when the
+        # manager has already stopped
+        new_batch_id = manager.send_request(number=1, url=url, web_key=site_key, captcha_type=captcha_type)
+        if new_batch_id is not None:
+            batch_id = new_batch_id
+
+        # You can now use the token to submit forms, etc. Sleep is used here instead to mock that
+        time.sleep(random.randint(5, 10))
+
+        # Stop manager if limit reached. Manager will completely stop only after all registered requests are solved
+        # as well (Use force_stop() to stop manager immediately instead).
+        count += 1
+        if count == total_run:
+            print('Manager stopped')
+            manager.stop()
+
+    # Stop the service process
+    service.stop()
+    service_proc.join()
+
+
+site_key = '6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-'
+captcha_type = 'v2'
+url = 'https://www.google.com/recaptcha/api2/demo'
+
+api_key = ''
+total_runs = 5
+
+# Remember to protect your main with this clause!
+if __name__ == "__main__":
+    main(total_runs)
