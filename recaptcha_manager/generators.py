e82b942ec87c6af2af5f2fd03dba67658e00347c
charxhit <charchit.a00@gmail.com>
2022-10-01 01:42:09 +0530

    Created placeholder modules to notify users of change in structure

--- recaptcha_manager/generators.py
+++ recaptcha_manager/generators.py
@@ -1,21 +1,3 @@
-import multiprocess as multiprocessing
-from ctypes import c_bool
+from recaptcha_manager import error
 
-def generate_queue(manager=None):
-    """
-    Generates a proxy object of class :class:`~multiprocessing.Queue`
-
-    :rtype: multiprocessing.Queue
-    """
-    if manager is None:
-        manager = multiprocessing.Manager()
-    return manager.Queue()
-
-
-def generate_flag():
-    """
-    Generates a shared boolean value using class :class:`~multiprocessing.Value`
-
-    :rtype: multiprocessing.sharedctypes.Synchronized
-    """
-    return multiprocessing.Value(c_bool, True)
+raise error
