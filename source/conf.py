2c81cfee5e5cc042d0898817fc96f1a13dca52e7
charxhit <charchit.a00@gmail.com>
2022-10-01 03:08:17 +0530

    Release for v0.0.8

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Charchit Agarwal'
 author = 'Charchit Agarwal'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.7'
+release = '0.0.8'
 
 
 # -- General configuration ---------------------------------------------------
