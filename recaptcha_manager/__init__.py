31c073b99b5d992485a7f1892fe389599265e651
charxhit <charchit.a00@gmail.com>
2022-10-01 01:40:53 +0530

    Added subpackage

--- recaptcha_manager/__init__.py
+++ recaptcha_manager/__init__.py
@@ -1,4 +1,25 @@
-from .generators import generate_queue
-from .manager import AutoManager, ManualManager
-from .services import AntiCaptcha, TwoCaptcha, CapMonster, BaseService
-from .exceptions import Exhausted
+"""If you're snooping around here, then know that the actual definitions for the below objects are present in
+recaptcha_manager.api sub-package. These placeholders are here only to inform users of the changes, since version
+0.0.7+ are not backwards compatible with the previous versions. They will be removed in a later version.
+
+Likewise, the modules present in this directory mimic the names of the actual implementations inside the sub-package,
+but will raise similar exceptions if use is attempted. """
+
+error = NotImplementedError("\n\nIt seems like you are using version 0.0.7+, but not importing from the correct "
+                                  "sub-package. Please import the relevant modules, functions and \nclasses from "
+                                  "recaptcha_manager.api instead. For example, to "
+                                  "correctly import AutoManager, do:\n\n    from recaptcha_manager.api import "
+                                  "AutoManager\n\n"
+                                  "Refer to the latest documentation here: https://recaptcha-manager.readthedocs.io/en/latest/")
+class _Deprecated(type):
+    def __getattr__(self, item):
+        raise error
+
+class AutoManager(metaclass=_Deprecated): pass
+
+
+ManualManager = AntiCaptcha = TwoCaptcha = CapMonster = BaseService = Exhausted = AutoManager
+
+def generate_queue(*args, **kwargs):
+    raise error
+
